9081417339a0b9667d84ff882aad0367af851afc
sergxlove <sergxlove@mail.ru>
2026-02-17 03:31:07 +0600

    add new functional

---
+++ BankingSystem/Requests/MonthsLeftRequest.cs
@@ -0,0 +1,7 @@
+﻿namespace BankingSystem.Requests
+{
+    public class MonthsLeftRequest
+    {
+        public int MaxMonthsLeft { get; set; }
+    }
+}
---
+++ BankingSystemDataAccess.Postgres/Dto/ClientBalanceDto.cs
@@ -0,0 +1,8 @@
+﻿namespace BankingSystemDataAccess.Postgres.Dto
+{
+    public class ClientBalanceDto
+    {
+        public string FullName { get; set; } = string.Empty;
+        public decimal Balance { get; set; }
+    }
+}
